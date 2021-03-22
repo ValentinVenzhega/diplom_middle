1dc382555399d3758425c3039cb89668a27a8b81
Valentin Venzhega <valek917@yandex.ru>
2021-03-22 22:10:53 +0300

    реализовал прилипание бургер меню

--- src/modules/toggleMenu.js
+++ src/modules/toggleMenu.js
@@ -1,6 +1,11 @@
 const toggleMenu = () => {
-   const popupMenu = document.querySelector('.popup-menu');
+   const popupMenu = document.querySelector('.popup-menu'),
+   menuBtn = document.querySelector('.menu-button'),
 
+   positionStart = menuBtn.offsetTop,
+   myStyle = document.querySelector('#my-style');
+
+   // открытие закрытие меню
    window.addEventListener('click', (event) => {
       const target = event.target;
       if (target.closest('.menu-button')) {
@@ -9,6 +14,36 @@ const toggleMenu = () => {
          popupMenu.style.display = 'none';
       } else if (target.closest('.popup-menu>ul>li>a')) {
          popupMenu.style.display = 'none';
+      }
+   });
+
+   // реализация прилипания бургер меню
+   if(!myStyle) {
+      const style = document.createElement('style');
+      style.setAttribute('id', 'my-style');
+      style.textContent = `
+         .position {
+            position: fixed;
+            top:0;
+            right: 14px;
+            z-index: 6000;
+         }
+         .top-menu {
+            paddin-top: 1px;
+            height: 59px;
+         }
+         .position + .popup-menu + .head-slider {
+            padding-top: 60px;
+         }
+      `;
+      document.head.appendChild(style);
+   }
+
+   window.addEventListener('scroll', (event) => {
+      if (window.pageYOffset > positionStart) {
+         menuBtn.classList.add("position");
+      } else {
+         menuBtn.classList.remove("position");
       }
    });
 };
