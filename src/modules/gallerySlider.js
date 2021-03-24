194a93faebe344d87627b02d914d83a9c2282afd
Valentin Venzhega <valek917@yandex.ru>
2021-03-24 19:41:58 +0300

    добавил погинацию в сладере галлерея

--- src/modules/gallerySlider.js
+++ src/modules/gallerySlider.js
@@ -1,27 +1,38 @@
 const gallerySlider = () => {
    const slider = document.querySelector('.gallery-slider'), 
       slide = slider.querySelectorAll('.slide'),
-      styleGallery = document.querySelector('#style-gallery');
+      styleGallery = document.querySelector('#style-gallery'),
+      galleryLine = document.querySelector('.gallery-line');
    
    let currentSlide = 0,
       interval;
 
-   const prevSlide = (elem, index) => {
-      elem[index].style.display = 'none';
+   const prevSlide = (elem, index, strClass) => {
+      elem[index].classList.remove(strClass);
    };
 
-   const nextSlide = (elem, index) => {
-      elem[index].style.display = 'block';
+   const nextSlide = (elem, index, strClass) => {
+      elem[index].classList.add(strClass);
    };
+
+   // добавление точек в разметку
+   for (let i = 0; i < slide.length; i++) {
+      const li = document.createElement('li');
+      li.className = 'line';
+      galleryLine.append(li);
+   }
+   let line = document.querySelectorAll('.line');
 
    // автопроигрывание слайдов
    const autoPlaySlide = () => {
-      prevSlide(slide, currentSlide);
+      prevSlide(slide, currentSlide, 'slide-active');
+      prevSlide(line, currentSlide, 'line-active');
       currentSlide++;
       if (currentSlide >= slide.length) {
          currentSlide = 0;
       }
-      nextSlide(slide, currentSlide);
+      nextSlide(slide, currentSlide, 'slide-active');
+      nextSlide(line, currentSlide, 'line-active');
    };
 
    const startSlide = (time = 3000) => {
@@ -36,14 +47,25 @@ const gallerySlider = () => {
    slider.addEventListener('click', (event) => {
       event.preventDefault();
 
+      // if (!target.matches('.gallery-btn, .line')) {
+      //    return;
+      // }
+
       const target = event.target;
-      prevSlide(slide, currentSlide);
+      prevSlide(slide, currentSlide, 'slide-active');
+      prevSlide(line, currentSlide, 'line-active');
 
       if (target.closest('#arrow-right')) {
          
          currentSlide++;
       } else if (target.closest('#arrow-left')) {
          currentSlide--;
+      }  else if (target.matches('.line')) {
+         line.forEach((elem, index) => {
+            if (elem === target) {
+               currentSlide = index;
+            }
+         });
       }
 
       if (currentSlide >= slide.length) {
@@ -54,7 +76,8 @@ const gallerySlider = () => {
          currentSlide = slide.length -1;
       }
 
-      nextSlide(slide, currentSlide);
+      nextSlide(slide, currentSlide, 'slide-active');
+      nextSlide(line, currentSlide, 'line-active');
    });
 
    slider.addEventListener('mouseover', (event) => {
