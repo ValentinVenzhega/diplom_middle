108e22be97d007b01b4464ee9585743da4351477
Valentin Venzhega <valek917@yandex.ru>
2021-03-26 22:21:14 +0300

    выбор клуба в footer обязательно

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -1,19 +1,42 @@
 const sendForm = () => {
 
-   const errorMessage = 'Что-то пошло не так',
-      form = document.querySelectorAll('form'),
+   const form = document.querySelectorAll('form'),
       statusMessage = document.createElement('div'),
       popup = document.querySelectorAll('.popup'),
       thanks = document.getElementById('thanks'),
-      freeVisitForm = document.querySelector('#free_visit_form'),
-      callbackForm = document.querySelector('#callback_form');
+      checkbox = document.querySelectorAll('input[type="checkbox"]'),
+      thanksContent = thanks.querySelector('.form-content'),
+      footerForm = document.getElementById('footer_form'),
+      clubFooter = footerForm.querySelectorAll('.club > input[type="radio"]'),
 
-   statusMessage.classList.add('status-message');
+      clubMessage = 'Пожалуйста, выберите клуб',
+      warningMessage = 'Необходимо подтвердить согласие на обработку персональных данных';
+
+   //проверка какой клуб выбран
+   let clubChecked = false;
+   
+   clubFooter.forEach((elem) => {
+      if(elem.checked) {
+         clubChecked = true;
+      }
+   });
+
+   if(!clubChecked) {
+      footerForm.addEventListener('click',(event)=>{
+         const target = event.target;
+
+         if(target.closest('.club')) {
+            statusMessage.textContent = '';
+            clubChecked = true;
+         }
+      });
+   }
 
    form.forEach (elem => {
       elem.addEventListener('submit', (event) => {
          event.preventDefault();
          elem.appendChild(statusMessage);
+         const target = event.target;
 
          statusMessage.textContent = 'идет отправка';
          
@@ -22,6 +45,17 @@ const sendForm = () => {
          formData.forEach((val, key) => { // берем значения из formData
             body[key] = val;
          });
+
+         //проверка на выбранный клуб
+         if(target.closest('#footer_form')) {
+            if(!clubChecked) {
+               statusMessage.textContent = clubMessage;
+               return;
+            } else {
+               statusMessage.textContent = '';
+               clubChecked = false;
+            }
+         }
 
          postData(body)
          .then((response) => {
@@ -38,9 +72,15 @@ const sendForm = () => {
 
          })
          .catch((error) => {
-            statusMessage.textContent = errorMessage;
+            statusMessage.textContent = '';
+            thanks.style.display = 'block';
+               thanksContent.innerHTML = `<div class="form-content">
+                  <h4>Извините!</h4>
+                  <p>Что-то пошло не так.<br> Ваша заявка не отправлена.</p>
+                  <button class="btn close-btn">Закрыть</button>
+               </div>`;  
             setTimeout(() => {
-               statusMessage.textContent = '';
+               thanks.style.display = 'block';
                popup.forEach(elem => elem.style.display = 'none');
             }, 2000);
          });
--- src/modules/validForm.js
+++ src/modules/validForm.js
@@ -1,38 +1,10 @@
 const validForm = () => {
    const form = document.querySelectorAll('form'),
-      checkbox = document.querySelectorAll('input[type="checkbox"]'),
-      footerForm = document.querySelector('#footer_form'),
-      radios = footerForm.querySelectorAll('input[type="radio"]'),
-      button = document.querySelectorAll('button[type="submit"]'),
-      footerBtn = document.querySelector('.callback-btn-footer'),
+
       regName = /^[А-Яа-я ]{2,20}$/,
       regPhone = /^(8|\+7)([0-9]{9,10})$/;
 
-      button.forEach(elem => elem.disabled = true);
-
-      // проверка выбран ли хоть один клуб
-      radios.forEach(item => {
-         item.addEventListener('click', function() {
-            for (let radio of radios) {
-               if (radio.checked) {
-                  footerBtn.disabled = false;
-               }
-            }
-         });
-      });
-      
-      // проверка нажат ли чекбокс
-      checkbox.forEach(item => {
-         item.addEventListener('click', () => {
-            if (item.checked) {
-               button.forEach(elem => elem.disabled = false);
-            } else {
-               button.forEach(elem => elem.disabled = true);
-            }
-         });
-      });
-   
-      // переводим каждое слово с большой буквы
+   // переводим каждое слово с большой буквы
    const substr = (elem) => {
       let words = elem.value.split(' ');
       
