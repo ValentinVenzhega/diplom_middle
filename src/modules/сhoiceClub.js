ced53029863016436947b30e4b068b51995d819d
Valentin Venzhega <valek917@yandex.ru>
2021-03-27 23:36:16 +0300

    исправил выпадющее меню

--- src/modules/сhoiceClub.js
+++ src/modules/сhoiceClub.js
@@ -1,14 +1,17 @@
 const choiceClub = () =>  {
    const clubSelect = document.querySelector('.club-select'),
-      listClub = clubSelect.querySelector('ul');   
+      listClub = document.querySelector('.left ul');   
    
    window.addEventListener('click', (event) => {
       const target = event.target;
       if (target.closest('.club-select')) {
          listClub.classList.toggle('list-active');
          
-      } else if (!target.closest('.club-select>ul')) {
+      } else if (!target.closest('ul')) {
          listClub.classList.remove('list-active');
+      }
+      if (target.closest('ul')) {
+         listClub.classList.add('list-active');
       }
    });
 };
