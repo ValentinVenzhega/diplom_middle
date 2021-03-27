4a7d0020ca0e1d52153f0cb0829558452ca89eae
Valentin Venzhega <valek917@yandex.ru>
2021-03-27 12:13:24 +0300

    исправил выпадающее меню с клубами

--- src/modules/сhoiceClub.js
+++ src/modules/сhoiceClub.js
@@ -5,10 +5,10 @@ const choiceClub = () =>  {
    window.addEventListener('click', (event) => {
       const target = event.target;
       if (target.closest('.club-select')) {
-         listClub.style.display = 'block';
+         listClub.classList.toggle('list-active');
          
       } else if (!target.closest('.club-select>ul')) {
-         listClub.style.display = 'none';
+         listClub.classList.remove('list-active');
       }
    });
 };
